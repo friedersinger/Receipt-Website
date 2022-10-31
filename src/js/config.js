b6d46364641e844a7215b935fa9bf06e49cbb8ee
friedersinger <frieder-singer@web.de>
2022-10-31 07:03:06 +0100

    `Inintial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6853d389-6c0e-4119-b77a-1fba91142a76';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,43 @@
+import View from './View.js';
+import previewView from './previewView.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query! Please try again ;)';
+  _message = '';
+
+  _generateMarkup() {
+    return this._data.map(result => previewView.render(result, false)).join('');
+  }
+}
+
+// MANUAL WAY TO GO
+// class ResultsView extends View {
+//   _parentElement = document.querySelector('.results');
+
+//   _generateMarkup() {
+//     console.log(this._data);
+//     return this._data.map(this._generateMarkupPreview).join('');
+//   }
+//   _generateMarkupPreview() {
+//     return `li class="preview">
+//     <a class="preview__link preview__link--active" href="#23456">
+//       <figure class="preview__fig">
+//         <img src="src/img/test-1.jpg" alt="Test" />
+//       </figure>
+//       <div class="preview__data">
+//         <h4 class="preview__title">Pasta with Tomato Cream ...</h4>
+//         <p class="preview__publisher">The Pioneer Woman</p>
+//         <div class="preview__user-generated">
+//           <svg>
+//             <use href="src/img/icons.svg#icon-user"></use>
+//           </svg>
+//         </div>
+//       </div>
+//     </a>
+//   </li>`;
+//   }
+// }
+
+export default new ResultsView();
